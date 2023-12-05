0b8ce935f85558cdd47c179b3408275e9d310fd2
CodeSandbox <support@codesandbox.io>
2023-12-05 01:55:56 +0000

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// src/main.(js|ts)
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Vueform from "@vueform/vueform";
+import vueformConfig from "./../vueform.config";
+
+const app = createApp(App);
+app.use(Vueform, vueformConfig);
+app.mount("#app");
